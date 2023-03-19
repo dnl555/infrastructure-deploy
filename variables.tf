f2ee9acf1c452e2bec5f554ea26910e3114a8c7d
dnl555 <dnl555@gmail.com>
2023-03-19 14:08:10 +0000

    + vpc

--- variables.tf
+++ variables.tf
@@ -2,14 +2,6 @@ variable "aws_account_id" {
   type = string
 }
 
-variable "aws_secret_access_key" {
-  type = string
-}
-
-
-variable "aws_access_key_id" {
-  type = string
-}
 
 variable "aws_region" {
   type = string
