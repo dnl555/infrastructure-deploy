22f72f2498608d85615b95726e53c9e2b527b68b
dnl555 <dnl555@gmail.com>
2023-03-19 14:26:40 +0000

    group

--- rds.tf
+++ rds.tf
@@ -1,3 +1,12 @@
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[1].id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage   = 10
   db_name             = "mydb"
@@ -7,5 +16,7 @@ resource "aws_db_instance" "default" {
   multi_az            = false
   username            = var.db_username
   password            = var.db_password
+  subnet_group_name   = aws_db_subnet_group.default.name
   skip_final_snapshot = true
 }
+
