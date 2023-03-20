c0a6c5cabab69d5f4aa27d017474c8e3c7a3ca40
dnl555 <dnl555@gmail.com>
2023-03-20 11:47:54 +0000

    fix db_engine

--- rds.tf
+++ rds.tf
@@ -11,8 +11,8 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   name                 = "mydb"
   engine               = "aurora-postgresql"
-  engine_version       = "12.8"
-  instance_class       = "db.t3.micro"
+  engine_version       = "11.15"
+  instance_class       = "db.t4g.medium"
   multi_az             = false
   username             = var.db_username
   password             = var.db_password
