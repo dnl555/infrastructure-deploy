164857e1cea0256c447fe7abdfd7050a11f032c4
dnl555 <dnl555@gmail.com>
2023-03-20 13:22:59 +0000

    route table

--- network.tf
+++ network.tf
@@ -123,5 +123,5 @@ resource "aws_route_table" "third_rt" {
 resource "aws_route_table_association" "private_subnet_asso" {
   count          = length(var.private_subnet_cidrs)
   subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
-  route_table_id = aws_route_table_private.third_rt.id
+  route_table_id = aws_route_table.third_rt.id
 }
