67e1da40a2cd4151c3634a8f3ed8f103fc9c55e4
dnl555 <dnl555@gmail.com>
2023-03-20 13:56:31 +0000

    rename natgateway

--- network.tf
+++ network.tf
@@ -139,7 +139,7 @@ resource "aws_route_table" "third_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.example.id
+    gateway_id = aws_nat_gateway.mynatgateway.id
   }
 
   tags = {
