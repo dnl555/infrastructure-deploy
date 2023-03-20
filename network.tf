5282c760d3889acfd44826f18e0b77ecf9292cbd
dnl555 <dnl555@gmail.com>
2023-03-20 13:21:13 +0000

    storage

--- network.tf
+++ network.tf
@@ -96,3 +96,32 @@ resource "aws_security_group" "alb-sg" {
     self             = false
   }]
 }
+
+resource "aws_nat_gateway" "example" {
+  subnet_id = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table_private" "third_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
+
+  tags = {
+    Name = "3rd Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table_private.third_rt.id
+}
